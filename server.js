783852156bd56a5d87baf91093bfef81acfcdc91
Timothy Cool <tcoolpg@gmail.com>
2019-03-12 20:39:21 -0700

    specify origins, dynamic port, basic response

--- server.js
+++ server.js
@@ -1,9 +1,30 @@
 var app = require('express')();
-var server = app.listen(3000, () => {
+
+app.set('port', (process.env.PORT || 3000));
+
+var allowedOrigins = [
+	'https://star-base.herokuapp.com/',
+	'http://localhost:8080',
+];
+
+app.get('/', (req, res) => {
+	res.send('Star Base Net is running...');
+});
+
+app.use(function(req, res, next) {
+	const origin = req.headers.origin;
+	if (allowedOrigins.includes(origin)) {
+		res.header("Access-Control-Allow-Origin", origin);
+		res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	}
+	next();
+});
+
+const server = app.listen(app.get('port'), () => {
 	console.log("I'm listening....");
 });
 
-var io = require('socket.io')(server);
+const io = require('socket.io')(server);
 io.on('connection', (socket) => {
 	console.log('connected to ' + socket.id);
 	socket.on('playerUpdate', (data) => {
