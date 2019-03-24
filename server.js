f85f008c5c22f9c23c14ec6bbb8bf10c36d00ac1
Timothy Cool <tcoolpg@gmail.com>
2019-03-24 13:14:26 -0700

    request players, have players join game room

--- server.js
+++ server.js
@@ -27,11 +27,22 @@ const server = app.listen(app.get('port'), () => {
 const io = require('socket.io')(server);
 io.on('connection', (socket) => {
 	console.log('connected to ' + socket.id);
+	socket.join('game');
 	socket.on('playerUpdate', (data) => {
 		socket.broadcast.emit('playerUpdate', data);
 	});
+	// Only applies to new player
 	socket.on('playerJoin', (data) => {
 		console.log(`broadcasting new player ${JSON.stringify(data)}`);
 		socket.broadcast.emit('playerJoin', data);
+	});
+	// New player requests old players
+	socket.on('requestPlayers', () => {
+		console.log(`${socket.id} requesting for the other players`);
+		io.to('game').emit('addPlayer', socket.id);
+	});
+	// Adds an existing player to a new players' game
+	socket.on('sendPlayer', (data) => {
+		io.to(`${data.client}`).emit('playerJoin', data.joiningPlayer);
 	});
 });
